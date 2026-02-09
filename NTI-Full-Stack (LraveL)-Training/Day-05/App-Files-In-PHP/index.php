d1ffdf5d26934fc778c6a1a6a75878e15aa8f806
A.Ismail Fathy <abdalrhmanismailfathiy@gmail.com>
2026-02-10 01:54:25 +0200

    Add files via upload

---
+++ NTI-Full-Stack (LraveL)-Training/Day-05/App-Files-In-PHP/index.php
@@ -0,0 +1,34 @@
+<?php
+
+// phpinfo();
+// $name="ali";
+// $gender='male';
+
+// echo(($name=='ali' || $gender=='male')&&($gender=='male' && $name=='ff'))? 'true':'false';
+
+//? **********************************
+
+// $grade;
+// if(grade=>90 && grade<=100){
+//     echo"A+";
+// } else if (grade<=90 && grade>=75){
+//     echo"b+";
+// }else if (grade>=60 && grade<=75){
+//     echo"c+";
+// }else if (grade>=50 && grade<=60){
+//     echo"d+";
+// }else{
+//     echo"c";
+// }
+
+
+//? **********************************
+
+for ($x = 1; $x <= 50; $x++) {
+    if($x%5==0){
+       echo"BOOM <br>"; 
+    }
+    else{
+    echo "$x <br>";
+}
+}
---
+++ NTI-Full-Stack (LraveL)-Training/Day-05/أكـــواد م.مـــروة/session_5/session_5/index.php
@@ -0,0 +1,80 @@
+<?php
+
+// echo "welcome"  ;
+
+// echo "marw <br>";
+// echo "marwa <br>";
+// echo "mara <br>";
+// 5+5 = 10 ;
+
+//variables
+//losly type  php
+//string   number float array object null boolean
+//int num = 5;
+//string name = "kkk
+
+// $firstName = 'ahmed';
+// $name = 22;
+// $isAdmin = true;
+// echo gettype($isAdmin);
+
+
+$age = 40;
+$name = "mohmed";
+// if($age > 50){
+//   if($name == "mohmed" ){
+//           echo "welcome ".$name;
+//   }
+
+// }
+// elseif($age > 30){
+// echo "welcome ".$age;
+// }
+// elseif($age > 30){
+// echo "welcome ".$name;
+// }
+// else{
+//     echo 'sorry';
+// }
+
+
+// if($age > 20): echo "welcome ".$name;
+// endif;
+
+// if($name == "ahmed") echo "welcome ".$name;
+
+// echo ' $age > 20?echo "welcome " : echo "sorry" ';
+// $gender = 'mail';
+
+//5    5 equal   ==  value 
+//===   value datatype
+
+// $num = "5";  
+// echo gettype($num). "<br>";
+// if($num === 5 ){
+//     echo "equal";
+// }
+
+// else{
+//     echo "not equal";
+// }
+
+
+
+//cating
+// $number  = (int)'mmmm' +5;     
+// echo ($number);
+
+// $number  = (int) 5.5 +5;     
+// echo ($number);
+
+$gender = 'mail';
+$name = "ahmed";
+
+
+if(($gender == 'mail'  || $name =="mohmed") &&  ($gender == 'mail'  && $name =="mm")) {
+    echo "true";
+}
+else{
+    echo "false";
+}
